d782d672d0e5a920630799a05b28f8f9e36f2188
Mirko Lecic <lecic.mirko@gmail.com>
2022-11-29 13:44:47 +0100

    Refactored AQI mapping

--- weather-app/src/app/air-polution/air-polution.component.ts
+++ weather-app/src/app/air-polution/air-polution.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { switchMap } from 'rxjs/operators';
 import { AirPolutionService } from '../air-polution.service';
 import { CurrentWeatherService } from '../current-weather.service';
+import { AIR_POLLUTION } from '../utils/constants';
 
 @Component({
   selector: 'app-air-polution',
@@ -12,6 +13,7 @@ export class AirPolutionComponent implements OnInit {
 
   response: any;
   responsePolution: any;
+  responsePolutionValue: string = "";
 
   constructor(private airPolutionService: AirPolutionService, 
               private currentWeatherService: CurrentWeatherService) { }
@@ -35,6 +37,7 @@ export class AirPolutionComponent implements OnInit {
       })
     ).subscribe((airPollutionInfo: any) => {
       this.responsePolution = airPollutionInfo;
+      this.responsePolutionValue = this.generatePollutionLabel(airPollutionInfo.list[0]?.main?.aqi);
     })
 
     // this.currentWeatherService.getCurrentCityWeather().subscribe(data => {
@@ -48,4 +51,23 @@ export class AirPolutionComponent implements OnInit {
   //     this.responsePolution = dataPol;
   //   });
   // }
+
+  generatePollutionLabel(aqi: number): string {
+    if(!aqi) return "";
+    let label;
+    switch(aqi) {
+      case AIR_POLLUTION.GOOD: label = "Good";
+      break;
+      case AIR_POLLUTION.FAIR: label = "Fair";
+      break;
+      case AIR_POLLUTION.MODERATE: label = "Moderate";
+      break;
+      case AIR_POLLUTION.POOR: label = "Poor";
+      break;
+      case AIR_POLLUTION.VERY_POOR: label = "Very Poor";
+      break;
+      default: label = "";
+    }
+    return label;
+  }
 }
--- weather-app/src/app/utils/constants.ts
+++ weather-app/src/app/utils/constants.ts
@@ -1,2 +1,10 @@
 export const API_APP_ID = '5637da4b8906e7d92ccc594c24b3a782';
 export const API_WEATHER_ENDPOINT = 'https://api.openweathermap.org/data/2.5/';
+
+export enum AIR_POLLUTION {
+    GOOD = 1,
+    FAIR,
+    MODERATE,
+    POOR,
+    VERY_POOR
+}
