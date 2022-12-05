75a574d14aff62386b11fe370373ecd747ce86a9
Lav Stefanovic <lavvstefanovic@gmail.com>
2022-12-05 11:03:28 +0100

    ID30 Refactored code

--- weather-app/src/app/current-weather/current-weather.component.ts
+++ weather-app/src/app/current-weather/current-weather.component.ts
@@ -11,9 +11,7 @@ import { CurrentWeatherService, CurrentWeather, Weather } from '../current-weath
 export class CurrentWeatherComponent implements OnInit, OnDestroy {
   response: CurrentWeather;
   iconValue: Weather["icon"];
-  descriptionValue: Weather["description"];
   imagePath = '';
-  imageDescription = '';
 
   /*
     currentCityWeatherSub is a Subsription, which will be 
@@ -38,9 +36,7 @@ export class CurrentWeatherComponent implements OnInit, OnDestroy {
     .subscribe((data: CurrentWeather) => { 
       this.response = data
       this.iconValue = this.response.weather[0].icon;
-      this.imagePath = `https://openweathermap.org/img/wn/${this.iconValue}@2x.png`, this.iconValue;
-      this.descriptionValue = this.response.weather[0].description;
-      this.imageDescription = `${this.descriptionValue}`, this.descriptionValue;
+      this.imagePath = `https://openweathermap.org/img/wn/${this.iconValue}@2x.png`;
     });
   }
 
